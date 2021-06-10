fcfcb557d1aa7a059a2d0387080cb2ef1a97ee57
Gralhix <74664918+Gralhix@users.noreply.github.com>
2021-06-10 12:31:28 +0100

    Update String1-first_half.py

--- String1-first_half.py
+++ String1-first_half.py
@@ -1,5 +1,7 @@
 # https://codingbat.com/prob/p107010
 # String-1 > first_half
+# Given a string name, e.g. "Bob", return a greeting of the form "Hello Bob!".
+
 
 def first_half(str):
     return str[0:len(str)//2]
