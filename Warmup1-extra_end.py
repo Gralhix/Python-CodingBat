357ee8d649c6053ef98017e15ae74c0f4019312e
Gralhix <74664918+Gralhix@users.noreply.github.com>
2021-06-15 08:53:56 +0100

    Delete Warmup1-extra_end.py

--- Warmup1-extra_end.py
+++
@@ -1,7 +0,0 @@
-# https://codingbat.com/prob/p148853
-# String-1 > extra_end
-# Given a string, return a new string made of 3 copies of the last 2 chars of the original string. The string length will be at least 2.
-
-def extra_end(str):
-  return (str[-2:])*3  
-
