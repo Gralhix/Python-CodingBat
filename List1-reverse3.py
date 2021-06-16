e17dbc46da94b02281546b956b86ef1951c0b4bb
Gralhix <74664918+Gralhix@users.noreply.github.com>
2021-06-16 15:14:55 +0100

    Add files via upload

---
+++ List1-reverse3.py
@@ -0,0 +1,6 @@
+# https://codingbat.com/prob/p192962
+# List-1 > reverse3
+# Given an array of ints length 3, return a new array with the elements in reverse order, so {1, 2, 3} becomes {3, 2, 1}.
+
+def reverse3(nums):
+    return [nums[2], nums[1], nums[0]]
