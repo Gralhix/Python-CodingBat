afa566a20d7f48ca8b2d921b08ac4d5108a351e5
Gralhix <74664918+Gralhix@users.noreply.github.com>
2021-06-16 11:16:37 +0100

    Add files via upload

---
+++ List1-same_first_last.py
@@ -0,0 +1,12 @@
+# https://codingbat.com/prob/p179078
+# List-1 > same_first_last
+# Given an array of ints, return True if the array is length 1 or more, and the first element and the last element are equal.
+
+def same_first_last(nums):
+    if len(nums) >= 1 and nums[0] == nums[-1]:
+        return True
+    else:
+        return False
+  
+  
+  
