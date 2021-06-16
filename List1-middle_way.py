f313055c25368b445df221c696e61e67b2b02953
Gralhix <74664918+Gralhix@users.noreply.github.com>
2021-06-16 15:49:56 +0100

    Add files via upload

---
+++ List1-middle_way.py
@@ -0,0 +1,6 @@
+# https://codingbat.com/prob/p171011
+# List-1 > middle_way
+# Given 2 int arrays, a and b, each length 3, return a new array length 2 containing their middle elements.
+
+def middle_way(a, b):
+    return [a[1], b[1]]  
