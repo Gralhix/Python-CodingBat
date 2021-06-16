9b414ba73de9b0390080b105d8f0272e47ea72a8
Gralhix <74664918+Gralhix@users.noreply.github.com>
2021-06-16 11:22:37 +0100

    Add files via upload

---
+++ List1-make_pi.py
@@ -0,0 +1,8 @@
+# https://codingbat.com/prob/p113659
+# List-1 > make_pi
+# Return an int array length 3 containing the first 3 digits of pi, {3, 1, 4}.
+
+def make_pi():
+    return [3,1,4]
+
+# Note: I'm not sure I completely understood what was required. Seems too obvious.
