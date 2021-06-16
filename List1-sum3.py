55f04c1dbc2ade15ae8cf788768cd918bc10ae1e
Gralhix <74664918+Gralhix@users.noreply.github.com>
2021-06-16 11:31:00 +0100

    Add files via upload

---
+++ List1-sum3.py
@@ -0,0 +1,6 @@
+# https://codingbat.com/prob/p191645
+# List-1 > sum3
+# Given an array of ints length 3, return the sum of all the elements.
+
+def sum3(nums):
+    return nums[0] + nums[1] + nums[2]  
