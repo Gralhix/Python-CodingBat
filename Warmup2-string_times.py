13d155282da7347bb80f4d6ffb90fe8c7593e91b
Gralhix <74664918+Gralhix@users.noreply.github.com>
2021-06-18 15:58:05 +0100

    Add files via upload

---
+++ Warmup2-string_times.py
@@ -0,0 +1,6 @@
+# https://codingbat.com/prob/p193507
+# Warmup-2 > string_times
+# Given a string and a non-negative int n, return a larger string that is n copies of the original string.
+
+def string_times(str, n):
+    return str * n
